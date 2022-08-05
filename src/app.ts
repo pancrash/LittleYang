576c7d582291207f2aeb88811d59b9bc628a9484
hebangjun <bangjun_he@163.com>
2022-08-05 15:00:48 +0800

    init

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,7 @@
-import { Component } from 'react'
+import React from 'react'
+import 'taro-ui/dist/style/index.scss'
 import './app.less'
 
-class App extends Component {
-
-  componentDidMount () {}
-
-  componentDidShow () {}
-
-  componentDidHide () {}
-
-  componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
-    return this.props.children
-  }
+export default function App ({children}: React.PropsWithChildren<{children:any}>) {
+  return children
 }
-
-export default App
